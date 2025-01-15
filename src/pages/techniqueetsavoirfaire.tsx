5ccf617973d0b87d03c4447d7c8c9da5b0ee1c40
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-01-15 01:38:46 +0100

    add techniqueetsavoirfaire Page

---
+++ src/pages/techniqueetsavoirfaire.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/navbar";
+
+export default function TechniqueEtSavoirFaire() {
+  return (
+    <div>
+      <Navbar />
+      <div className="mt-48 mx-4 md:mx-32 md:mt-64">
+        <h1>test</h1>
+      </div>
+    </div>
+  );
+}
