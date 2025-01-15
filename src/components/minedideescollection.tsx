a5440ab87405080d6a849b5d3164778946f73ee9
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-01-15 01:37:22 +0100

    add idea page with collection and details

---
+++ src/components/minedideescollection.tsx
@@ -0,0 +1,88 @@
+import Link from "next/link";
+import { useCollection } from "@/hooks/use-collection";
+import { CollectionType } from "@/types/collectionType";
+import { useState, ReactElement } from "react";
+import DOMPurify from "dompurify";
+import Image from "next/image";
+
+type MinedideesCollectionProps = {
+  className: string;
+  innerDivClassName: string;
+  showInnerText: boolean;
+  imageClassName: string;
+  nameClassName: string;
+  collectionId: string;
+  slug?: string;
+  svgElement?: ReactElement;
+  priceClassName?: string;
+};
+
+export default function MinedideesCollection({
+  className,
+  innerDivClassName,
+  showInnerText = true,
+  imageClassName,
+  nameClassName,
+  svgElement,
+  priceClassName,
+  collectionId,
+}: MinedideesCollectionProps) {
+  const collection = useCollection(
+    collectionId || process.env.NEXT_PUBLIC_MINE_DIDEES_COLLECTION_ID || ""
+  );
+  const [visibleTextIds, setVisibleTextIds] = useState<{
+    [key: string]: boolean;
+  }>({});
+
+  const toggleTextVisibility = (id: string) =>
+    setVisibleTextIds((prevState) => ({ ...prevState, [id]: !prevState[id] }));
+
+  return (
+    <div className={className}>
+      {collection.map((item: CollectionType) => (
+        <div key={item.id} className="collection-item relative cursor-pointer">
+          {svgElement && (
+            <div
+              className="absolute top-0 right-0 m-2 z-50 block md:hidden"
+              onClick={() => toggleTextVisibility(item.id)}
+            >
+              {svgElement}
+            </div>
+          )}
+          <Link href={`/minedideesdetails/${item?.fieldData?.slug}`}>
+            <div className="relative group">
+              {item.fieldData["thumbnail-image"] && (
+                <Image
+                  src={item.fieldData["thumbnail-image"].url}
+                  alt={item.fieldData["thumbnail-image"].alt || "Image"}
+                  width={1000}
+                  height={1000}
+                  className={` ${imageClassName} object-cover `}
+                />
+              )}
+
+              <div
+                className={`${innerDivClassName} ${
+                  visibleTextIds[item.id] ? "opacity-100" : "opacity-0"
+                } transition-opacity`}
+              >
+                {showInnerText && (
+                  <div
+                    className="mx-4"
+                    dangerouslySetInnerHTML={{
+                      __html: DOMPurify.sanitize(item.fieldData["texte"] || ""),
+                    }}
+                  />
+                )}
+              </div>
+            </div>
+            <p className={nameClassName}>{item.fieldData.name}</p>
+            {item.fieldData.price && (
+              <p className={priceClassName}>{item.fieldData.price}€</p>
+            )}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/pages/minedideesdetails/[slug].tsx
@@ -0,0 +1,91 @@
+import Navbar from "@/components/navbar";
+import { useItem } from "@/hooks/use-item";
+import { useRouter } from "next/router";
+import DOMPurify from "dompurify";
+import Footer from "@/components/footer";
+import MinedideesCollection from "@/components/minedideescollection";
+import Scene from "@/components/Scene";
+
+export default function Page() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  if (!router.isReady || !slug) return <div>Loading...</div>;
+
+  return <MineDetails slug={slug as string} />;
+}
+
+function getModelPath(itemId: string): string {
+  const modelMap: { [key: string]: string } = {
+    "6771fc37bb8eac98d7adaf65": "/chaise3D.glb",
+    "6771fc37bb8eac98d7adaf66": "/chaisezèbre3D.glb",
+    "677d7a7d91f5ec1586cc24e2": "/clubartdéco3D.glb",
+    "677d7ab23a04a483eb61cb59": "/carcassemédaillon.glb",
+  };
+
+  // Default to carcasse médaillon if no match is found
+  return modelMap[itemId] || "/chaise3D.glb";
+}
+
+function MineDetails({ slug }: { slug: string }) {
+  const item = useItem(
+    process.env.NEXT_PUBLIC_MINE_DIDEES_COLLECTION_ID || "",
+    slug
+  );
+
+  if (!item) return <div>Loading...</div>;
+
+  const mainImage = item.fieldData["thumbnail-image"];
+  const modelPath = getModelPath(item.id);
+
+  return (
+    <div>
+      <Navbar />
+      <div className="mt-48 md:mt-64 md:flex  md:mx-44">
+        <div className="w-full md:w-1/3">
+          <h1>{item.name}</h1>
+          {mainImage && (
+            <div className="flex justify-center items-center relative rounded-lg h-[550px]">
+              <Scene modelPath={modelPath} />
+            </div>
+          )}
+        </div>
+        <div className="w-full md:w-2/3 ">
+          <div className="flex flex-col mt-4 mx-4 md:ml-10 md:mr-36 md:mt-32">
+            <div className="md:border-l-2 md:border-gray-300 md:pl-2">
+              {/* BOX TEXTE */}
+              <div
+                className="text-xl font-semibold text-center md:text-left"
+                /* BOX NOM CHAISE */
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(item.fieldData["name"]),
+                }}
+              />
+              <div
+                style={{ fontSize: "20px", lineHeight: "24px" }}
+                className="text-base mt-4 text-gray-500"
+                /* BOX BLOG CONTENT */
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(
+                    item.fieldData["description"] || ""
+                  ),
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-10 border-t-2 border-gray-300 mx-20 md:my-20 md:mx-36" />
+      <MinedideesCollection
+        collectionId={process.env.NEXT_PUBLIC_MINE_DIDEES_COLLECTION_ID || ""}
+        className="mt-16 grid grid-cols-3 gap-4 mx-4 md:grid-cols-6 md:mx-28"
+        nameClassName="font-light text-sm text-center"
+        imageClassName="w-full rounded-lg"
+        innerDivClassName="hidden"
+        showInnerText={false}
+        priceClassName="hidden"
+      />
+      <Footer />
+    </div>
+  );
+}
