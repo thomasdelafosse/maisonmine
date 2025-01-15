0d6a8e71b6d82bc49398e716ab446e304caa0a2b
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-01-15 01:30:37 +0100

    update project configuration files

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  transpilePackages: ["three"],
+  images: {
+    domains: ["cdn.prod.website-files.com"],
+  },
 };
 
 export default nextConfig;
