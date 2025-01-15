51cd00f56f1116da87335f95ca9ae8b2a375b006
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-01-15 01:38:00 +0100

    Add collectionType

---
+++ src/types/collectionType.ts
@@ -0,0 +1,97 @@
+export type CollectionType = {
+  id: string;
+  name: string;
+  slug: string;
+  createdOn?: string;
+  fieldData: {
+    name: string;
+    slug?: string;
+    price?: string;
+    date?: string;
+    description?: string;
+    "blog-post-summary"?: string;
+    "thumbnail-image"?: {
+      url: string;
+      alt?: string;
+    };
+    floorplan?: {
+      url: string;
+      alt?: string;
+    };
+    texte?: string;
+    "blog-content"?: string;
+    "property-description"?: string;
+    "mini-1"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-2"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-3"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-4"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-5"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-6"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-7"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-8"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-9"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-10"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-11"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-12"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-13"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-14"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-15"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-16"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-17"?: {
+      url: string;
+      alt?: string;
+    };
+    "mini-18"?: {
+      url: string;
+      alt?: string;
+    };
+  };
+};
