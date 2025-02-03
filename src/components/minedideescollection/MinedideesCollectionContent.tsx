93607bdcedc12c9ed01cccb366ea8f48b7efda88
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-02-03 01:11:46 +0100

    Fix mine d'idée since we don't use webflow CMS

--- src/components/minedideescollection/MinedideesCollectionContent.tsx
+++ src/components/minedideescollection/MinedideesCollectionContent.tsx
@@ -2,18 +2,7 @@ import MinedideesCollection from "@/components/minedideescollection";
 import { useCollection } from "@/hooks/use-collection";
 
 export default function MinedideesCollectionContent() {
-  const collection = useCollection(
-    process.env.NEXT_PUBLIC_MINE_DIDEES_COLLECTION_ID || ""
-  );
-
-  if (!collection.length) {
-    return (
-      <div className="flex justify-center items-center min-h-[50vh]">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
-      </div>
-    );
-  }
-
+  
   return (
     <div className="relative -z-50">
       <MinedideesCollection
