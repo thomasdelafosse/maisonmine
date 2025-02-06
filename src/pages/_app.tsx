7de59fd1b1426aa041bbcb4d7e30334c9833584e
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-02-06 18:06:00 +0100

    Add title "maison mine"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,6 +19,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>Maison Mine</title>
+        <link rel="icon" href="/favicon.ico" />
         <meta
           name="viewport"
           content={`width=device-width, initial-scale=1${
