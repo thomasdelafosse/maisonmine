687ef18b3e692fe38516068368e2d04d60b591b8
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-11-14 05:13:47 +0100

    rename une-mine-de-savoir-faire

---
+++ src/app/une-mine-de-savoir-faire/page.tsx
@@ -0,0 +1,5 @@
+import MineDeSavoirFaireContent from "@/components/features/minedesavoirfaire/MineDeSavoirFaireContent";
+
+export default function UneMineDeSavoirFaire() {
+  return <MineDeSavoirFaireContent />;
+}
