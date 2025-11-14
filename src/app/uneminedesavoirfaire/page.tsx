02329db76f2d0ec4033e7ae59c36ba060b6a6b27
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-11-14 05:15:25 +0100

    delete files

--- src/app/uneminedesavoirfaire/page.tsx
+++
@@ -1,5 +0,0 @@
-import MineDeSavoirFaireContent from "@/components/features/minedesavoirfaire/MineDeSavoirFaireContent";
-
-export default function uneMineDeSavoirFaire() {
-  return <MineDeSavoirFaireContent />;
-}
--- src/components/features/minedechanges/components/MinedechangesCollection/MinedechangesCollectionContent.tsx
+++
@@ -1,9 +0,0 @@
-import MinedechangesCollection from "./MinedechangesCollection";
-
-export default function MinedechangesCollectionContent() {
-  return (
-    <div className="relative">
-      <MinedechangesCollection showInnerText={true} />
-    </div>
-  );
-}
