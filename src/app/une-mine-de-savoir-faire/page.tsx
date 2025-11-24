4f13804db243a90ddb880901df8e36f7b67d1b37
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-11-24 23:19:19 +0100

    mine-de-savoir-faire : add metadata

--- src/app/une-mine-de-savoir-faire/page.tsx
+++ src/app/une-mine-de-savoir-faire/page.tsx
@@ -1,4 +1,11 @@
 import MineDeSavoirFaireContent from "@/components/features/minedesavoirfaire/MineDeSavoirFaireContent";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Une Mine de Savoir-Faire | Maison Mine",
+  description:
+    "Découvrez nos réalisations et notre savoir-faire en tapisserie d'ameublement. Une galerie de nos créations et restaurations.",
+};
 
 export default function UneMineDeSavoirFaire() {
   return <MineDeSavoirFaireContent />;
