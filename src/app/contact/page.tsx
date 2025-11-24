7be56733484672455e555590b41631eab11a7c15
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-11-24 23:18:51 +0100

    contact : add metadata

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,11 @@
 import { ContactForm } from "@/components/features/contact/ContactForm";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact | Maison Mine",
+  description:
+    "Contactez-nous pour vos projets de rénovation de sièges. Demandez un devis ou des informations complémentaires.",
+};
 
 export default async function Contact() {
   return <ContactForm />;
